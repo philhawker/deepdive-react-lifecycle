bc8e2119413ee89cc8c506649dc5137f08a95fde
Phil Hawker <philhawker1211@gmail.com>
2020-02-24 11:24:27 -0700

    set up routes

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import Discussion from './discussion';
-import Rules from './rules';
-import Workflow from './workflow';
+
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>DeepDive Lifecycle</h1>
-        <Discussion />
-        <Rules />
-        <Workflow />
       </div>
     );
   }
