175e1d63cdf121f58623846ac97a6804852634f2
Phil Hawker <philhawker1211@gmail.com>
2020-02-24 12:14:49 -0700

    added start stop datafeed lifecycle

--- src/components/discussion.js
+++ src/components/discussion.js
@@ -3,11 +3,35 @@ import React, { Component } from 'react'
 export default class Discussion extends Component {
     constructor() {
         super()
+
+        this.state = {
+            pageTitle: 'Discussion',
+            currentTime: String(new Date())
+        }
     }
+
+
+    // use this to stop a data feed
+    componentDidMount() {
+        this.liveTime = setInterval(() => {
+            console.log('new chat message')
+
+            this.setState({ currentTime: String(new Date()) })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.liveTime)
+    }
+    // use this to stop a data feed
+
     render() {
+        const { pageTitle, currentTime } = this.state
+
         return (
             <div>
-                Discussion...
+                <h1>{pageTitle}</h1>
+                <div>{currentTime}</div>
             </div>
         )
     }
